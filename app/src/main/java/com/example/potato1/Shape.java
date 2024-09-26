99407d9ea05a1b8542ce542bcb0695ceeb63c83d
YoniKAr <hirdaesi@ualberta.ca>
2024-09-25 22:41:25 -0600

    Made new Android Project with Team Name

---
+++ app/src/main/java/com/example/potato1/Shape.java
@@ -0,0 +1,10 @@
+package com.example.potato1;
+
+public abstract class Shape {
+    protected int x;
+    protected int y;
+
+    public abstract double getArea();
+
+    public abstract double getPerimeter();
+}
