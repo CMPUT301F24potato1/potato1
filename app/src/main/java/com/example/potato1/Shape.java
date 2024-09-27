a024222e0dabc0e0df37c12a601cc762c37ab84f
Simi Kamali <simikamali@Simis-MacBook-Pro.local>
2024-09-26 19:02:50 -0600

    resolved conflict

--- app/src/main/java/com/example/potato1/Shape.java
+++ app/src/main/java/com/example/potato1/Shape.java
@@ -3,7 +3,11 @@ package com.example.potato1;
 public abstract class Shape {
     protected int x;
     protected int y;
+<<<<<<< HEAD
     protected String color = "orange";
+=======
+    protected String color = "blue";
+>>>>>>> c4c03e0 (added color field)
 
     public abstract double getArea();
 
