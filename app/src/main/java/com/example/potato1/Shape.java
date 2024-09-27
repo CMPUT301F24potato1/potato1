7495c6384842851ae9501bbac3c3c0d3352ddc4e
Xavier Quintanilla <113614470+Xavialess@users.noreply.github.com>
2024-09-26 18:32:25 -0600

    Added the String color field "orange" in main
    As a member, on your local machine:
    
    Pull the main branch from GitHub. You should have a conflict. (Origin/main -> local your-own)
    
    Resolve the conflict.
    Commit the change.
    Push the branch to GitHub.

--- app/src/main/java/com/example/potato1/Shape.java
+++ app/src/main/java/com/example/potato1/Shape.java
@@ -3,6 +3,7 @@ package com.example.potato1;
 public abstract class Shape {
     protected int x;
     protected int y;
+    protected String color = "orange";
 
     public abstract double getArea();
 
